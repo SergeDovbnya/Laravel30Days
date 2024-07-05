dd38d7ece48d48c3c5531ed26ba5617e52ca14d9
SergeDovbnya <serge.dovbnya@gmail.com>
2024-07-05 13:20:48 +0200

    Day 5
    Styling navbar links

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,1 +1,7 @@
-<a {{$attributes}} >{{ $slot }}</a>
+@props(['active'])
+
+<a 
+    class=" {{ request()->is('/') ? 'g-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium" 
+    aria-current="{{ request()->is('/') ? 'page' : 'false'}}"
+    {{ $attributes }}
+> {{ $slot }} </a>
